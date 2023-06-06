9ccd3ecd6c619e8ad56792ee2c8e6a994fb23858
aldgonza <aldogo95@hotmail.com>
2023-06-06 15:47:07 +0200

    binary conv working

--- src/client/client.c
+++ src/client/client.c
@@ -12,6 +12,25 @@
 
 #include "../../include/minitalk.h"
 
+void	ft_chartobin(int *binaryarray, char *chararray, int length)
+{
+    int i = 0;
+	int	j = 7;
+	char ch;
+    while (i < length && chararray[i] != '\0')
+	{
+		j = 7;
+        ch = chararray[i];
+        while (j >= 0)
+		{
+            binaryarray[i * 8 + (7 - j)] = (ch >> j) & 1;
+            j--;
+        }
+        i++;
+    }
+	return ;
+}
+
 void    send_string(pid_t pid, const char* str) 
 {
     int len;
@@ -34,10 +53,33 @@ void    send_string(pid_t pid, const char* str)
 int ft_client(int argc, char **argv)
 {
     int pid;
+    int *binarray;
+    int len;
+    int i;
+    int j;
 
+    i = 0;
+    j = 1;
+    len = ft_strlen(argv[2]);
+    if (argc < 3)
+        return (0);
+    ft_printf("len es: %i\n", len);
+    binarray = malloc(len * 8);
     pid = ft_atoi(argv[1]);
+    ft_chartobin(binarray, argv[2], len);
     //send_string(pid, argv[2]); // Send string to child process
     kill(pid, SIGTERM); // Terminate child process
-    ft_printf("cantidad de variables: %i\nCon esta array: %s\n", argc, argv[1]);
+    //ft_printf("cantidad de variables: %i\nCon esta array: %s\n\n", argc, argv[2]);
+    while (i < len * 8)
+    {
+        ft_printf("%d", binarray[i]);
+        if (j == 8)
+        {
+            ft_printf(" ");
+            j = 0;
+        }
+        i++;
+        j++;
+    }
     return(0);
 }
--- src/server/server.c
+++ src/server/server.c
@@ -12,45 +12,12 @@
 
 #include "../../include/minitalk.h"
 
-static void	action(int sig, siginfo_t *info, void *context)
-{
-	static int				i = 0;
-	static pid_t			client_pid = 0;
-	static unsigned char	c = 0;
-
-	(void)context;
-	if (!client_pid)
-		client_pid = info->si_pid;
-	c |= (sig == SIGUSR2);
-	if (++i == 8)
-	{
-		i = 0;
-		if (!c)
-		{
-			kill(client_pid, SIGUSR2);
-			client_pid = 0;
-			return ;
-		}
-		ft_putchar_fd(c, 1);
-		c = 0;
-		kill(client_pid, SIGUSR1);
-	}
-	else
-		c <<= 1;
-}
-
 int ft_server()
 {
     int pid;
-    struct sigaction    act;
 
     pid = getpid();
     ft_printf("Waiting for signal on PID: %i...\n", pid);
-    //ft_memset(&act, 0, sizeof(act));
-    act.sa_sigaction = action;
-    act.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
     while (1)
     {
       sleep(1); 
